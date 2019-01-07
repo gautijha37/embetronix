4f9c91a977c794ae4f855f92f4f4db8803066f34
gautijha37 <40668787+gautijha37@users.noreply.github.com>
2019-01-07 15:33:23 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,39 @@
 // File for Initializing maze, then updating maze and moving bot to explore the maze and finally doing the sprint run
-
+/*     y ^
+         |
+         |
+         |
+       (0,0)-------> x
+*/
 #include<stdio.h>
 
 struct cord{         //Coordinate of a Cell
   int x,y;
   };
+
+struct bot_parameters{
   
+  struct cord bot_cord;
+  int bot_moving_dir;  //0,1,2,3 for Bot moving in North,East,West,South
+  
+  };
+
+struct bot_parameters bot;
+bot.bot_cord.x=0;
+bot.bot_cord.y=0;
+bot.bot_moving_dir=0;    //Initially moving North
+
 struct grid{            //One unit of Maze
 
   int value;            //Value of Cell by Floodfill
   int count;            //No. of times cell has already been visited
-  bool wallPresent[4];   //0,1,2,3 for Wall present at North, East, West, South
+  bool wallpresent[4];   //0,1,2,3 indices for Wall present at North, East, West, South. 1 if wall present, 0 otherwise.
   
   struct cord grid_cord;
-  struct cord prev_grid_cord;
+  struct cord prev_grid_cord;         //this may not be needed... m not sure
   struct cord neighbour_cord[4];     //Neighbour in North,East,West,South
   
   }
-
-int bot_moving_dir=0;  //0,1,2,3 for Bot moving in North,East,West,South
 
 struct grid maze[16][16];
 
@@ -36,28 +51,38 @@ void neighbours_assign()
      }
 }  
 
-void maze_setup()
+void maze_setup()                      //Initializing grid variables and values with potential value algo
 {
      int i,j;
-     for(i=0;i<16;i++)
+     for(i=0;i<16;i++)               //Setting potential values for all grids
      for(j=0;j<16;j++)
      {
                if((i<8)&&(j<8))       maze[i][j].value=14-j-i;
                if((i>=8)&&(j<8))      maze[i][j].value=maze[15-i][j].value;  
                if((i<8)&&(j>=8))      maze[i][j].value=maze[i][15-j].value;
-               if((i>=8)&&(j>=8))     maze[i][j].value=maze[15-i][15-j].value;               
-     
+               if((i>=8)&&(j>=8))     maze[i][j].value=maze[15-i][15-j].value;     
+               maze[i][j].count=0;
+               maze[i][j].grid_cord.x=i;
+               maze[i][j].grid_cord.y=j;
      }
+     
+     for(i=0;i<16;i++){                           //Setting boundary walls
+        maze[0][i].wallpresent[1]=1;
+        maze[15][i].wallpresent[2]=1;
+        maze[i][0].wallpresent[3]=1;
+        maze[i][15].wallpresent[0]=1;
+     }
+       
 }
 
 void dead_end_check(struct grid a){                               //checks dead end of grids adjacent to grid a
   int i,x;
-  if(a.grid_cord.x==0&&a.grid_cord.y==1)                          //If grid to check is not the (0,1) grid whose south is Start Grid
+  if(a.grid_cord.x==0&&a.grid_cord.y==1)                          //If grid to check is the (0,1) grid whose south is Start Grid then abort
     return;
   else{
     for(i=0;i<4&&i!=bot_moving_dir;i++){
       x=a.neighbour_cord[i].wallpresent[0]+a.neighbour_cord[i].wallpresent[1]+a.neighbour_cord[i].wallpresent[2]+a.neighbour_cord[i].wallpresent[3];
-      if(x==3)
+      if(x==3)                               //if adjacent grid has 3 walls then put fourth wall also
         a.neighbour_cord[i].wallpresent={1,1,1,1};
     }
   }  
