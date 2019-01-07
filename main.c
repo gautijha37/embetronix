278caa53842517b6ef1373b6e015ecc4f3492e68
gautijha37 <40668787+gautijha37@users.noreply.github.com>
2019-01-07 09:24:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -11,17 +11,15 @@ struct grid{            //One unit of Maze
   int value;            //Value of Cell by Floodfill
   int count;            //No. of times cell has already been visited
   bool wallPresent[4];   //0,1,2,3 for Wall present at North, East, West, South
-  int bot_moving_dir;  //4 bit no. 0b1000 for moving North
-                        //          0b0100 for East
-                        //          0b0010 for West
-                        //          0b0001 for South
   
   struct cord grid_cord;
   struct cord prev_grid_cord;
   struct cord neighbour_cord[4];     //Neighbour in North,East,West,South
   
   }
-  
+
+int bot_moving_dir=0;  //0,1,2,3 for Bot moving in North,East,West,South
+
 struct grid maze[16][16];
 
 void neighbours_assign()
@@ -57,7 +55,7 @@ void dead_end_check(struct grid a){                               //checks dead 
   if(a.grid_cord.x==0&&a.grid_cord.y==1)                          //If grid to check is not the (0,1) grid whose south is Start Grid
     return;
   else{
-    for(i=0;i<4;i++){
+    for(i=0;i<4&&i!=bot_moving_dir;i++){
       x=a.neighbour_cord[i].wallpresent[0]+a.neighbour_cord[i].wallpresent[1]+a.neighbour_cord[i].wallpresent[2]+a.neighbour_cord[i].wallpresent[3];
       if(x==3)
         a.neighbour_cord[i].wallpresent={1,1,1,1};
